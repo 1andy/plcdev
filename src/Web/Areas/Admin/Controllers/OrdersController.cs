f51bb4bad6a055faf81bf054967fb4c6eddf5fe0
1A <evgeny.tukin@gmail.com>
2011-04-02 22:31:04 +0300

    Admin/Orders

---
+++ src/Web/Areas/Admin/Controllers/OrdersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PlexCommerce.Web.Areas.Admin.Controllers
+{
+    public class OrdersController : AdminControllerBase
+    {
+        public ActionResult Index()
+        {
+            var model = new OrderIndexViewModel();
+            return View(model);
+        }
+    }
+}
