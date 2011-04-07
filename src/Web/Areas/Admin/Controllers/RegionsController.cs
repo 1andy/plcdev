51626819ed39eb916e266dc8563878fc07fc99c2
1A <evgeny.tukin@gmail.com>
2011-04-07 09:14:35 +0300

    Settings and Regions

---
+++ src/Web/Areas/Admin/Controllers/RegionsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NHibernate;
+
+namespace PlexCommerce.Web.Areas.Admin.Controllers
+{
+    public class RegionsController : AdminControllerBase
+    {
+        private readonly ISession _session;
+
+        #region ctor
+
+        public RegionsController(ISession session)
+            : base(session)
+        {
+            _session = session;
+        }
+
+        #endregion
+
+        public ActionResult Index()
+        {
+            var model = new RegionsIndexViewModel();
+
+            return View(model);
+        }
+    }
+}
---
+++ src/Web/Areas/Admin/Controllers/SettingsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NHibernate;
+
+namespace PlexCommerce.Web.Areas.Admin.Controllers
+{
+    public class SettingsController : AdminControllerBase
+    {
+        private readonly ISession _session;
+
+        #region ctor
+
+        public SettingsController(ISession session)
+            : base(session)
+        {
+            _session = session;
+        }
+
+        #endregion
+
+        public ActionResult Index()
+        {
+            var model = new SettingsIndexViewModel();
+
+            return View(model);
+        }
+    }
+}
